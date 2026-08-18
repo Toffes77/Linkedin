76a76dea375076a6a3a284d886967bccae5f63b1
Juan Cruz Maletti <juan.maletti@philips.edu.ar>
2026-08-17 22:09:59 -0300

    Correcion de app (guia -> real)(el crack que escribe esto es el juancho)

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,19 +1,23 @@
 from fastapi import FastAPI
 
 from src.middlewares.error_middleware import app_error_handler
-from src.routers import auth_router, user_router, pasajero_router, conductores_router
+from src.routers import auth_router, usuario_router, empresa_router, experiencia_router, conexion_router, publicacion_router, reacciones_router, feed_router, oferta_router, postulacion_router
 from src.utils.errors import AppError
 
 app = FastAPI(title="Initial Structure API")
 
 app.add_exception_handler(AppError, app_error_handler)
 
-app.include_router(user_router.router, prefix="/api")
+app.include_router(usuario_router.router, prefix="/api")
 app.include_router(auth_router.router, prefix="/api")
-app.include_router(pasajero_router.router, prefix="/api")
-app.include_router(conductores_router.router, prefix="/api")
-# TODO: registrar product_router cuando se implemente
-# app.include_router(product_router.router, prefix="/api")
+app.include_router(empresa_router.router, prefix="/api")
+app.include_router(experiencia_router.router, prefix="/api")
+app.include_router(conexion_router.router, prefix="/api")
+app.include_router(publicacion_router.router, prefix="/api")
+app.include_router(reacciones_router.router, prefix="/api")
+app.include_router(feed_router.router, prefix="/api")
+app.include_router(oferta_router.router, prefix="/api")
+app.include_router(postulacion_router.router, prefix="/api")
 
 
 @app.get("/health")
